67e0e5265de9b1a5a7eb46d03892164fe4fccd39
Thinh Pham <mrlordkaj@gmail.com>
2020-03-03 21:03:06 +0700

    Initial Commit.

---
+++ MPU6050/examples/demo_3d/nbproject/Makefile-variables.mk
@@ -0,0 +1,27 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=/home/pi
+CND_ARTIFACT_NAME_Release=demo_3d
+CND_ARTIFACT_PATH_Release=/home/pi/demo_3d
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=demo3d.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/demo3d.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
